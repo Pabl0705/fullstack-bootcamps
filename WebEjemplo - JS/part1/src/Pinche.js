afd9ffb4c8ea9f90bd56b11d7cfd908e71120702
Pabl0705 <165886221+Pabl0705@users.noreply.github.com>
2024-10-10 10:25:33 +0200

    Delete WebEjemplo - JS directory

--- WebEjemplo - JS/part1/src/Pinche.js
+++
@@ -1,7 +0,0 @@
-const Pinche = (props) => {
-    return <h2 
-    style={{color: props.color}}> {/* El estilo se ha de pasar como objeto */}
-        {props.message}</h2>   /* Estamos cogiendo los atributos "message" que se utilicen en el codigo */
-}
-
-export default Pinche;
